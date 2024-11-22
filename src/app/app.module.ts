d345a324ed22feb007faaee92e1889331b9ce4a0
juanca0108 <109818872+juanca2805@users.noreply.github.com>
2024-11-22 18:31:15 -0500

    Se crean unos componentes de prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
+import { ResultsComponent } from './pages/results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
